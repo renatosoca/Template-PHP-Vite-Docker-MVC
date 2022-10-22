905a9bd14ea2cc9a5b5b19ecd9a19d72f5cdd7f3
Bleyomm <bleyomm@gmail.com>
2022-10-21 22:43:26 -0500

    Update formPlanes.php

--- controlador/formPlanes.php
+++ controlador/formPlanes.php
@@ -23,9 +23,9 @@
         $datos = $formPlanes->guardarPlan($nombrePlan,$costoPlan,$duracionPlan,$descripcionPlan,$fecha_creacion,$hora_creacion,$nombre_creador);
         
         if($datos == '1'){
-            echo "Se inserto los datos a la base con éxito.";
+            echo "1";
         }else{
-            echo "No hubo éxito al insertar los datos en la base.";
+            echo "0";
         }
 
         unset($datos);
@@ -46,12 +46,13 @@
                     <td class='table__td'>".$row["descripcion"]."</td>
                     <td class='table__td'>
                         <div class='table__btn'>
-                            <a href='../controlador/formPlanes.php?proceso=editarPlan&id=".$row["id"]."' class='icon'>
-                                <i class='fa-solid fa-pen-to-square btn__edit'></i>
-                            </a>
-                            <a href='../controlador/formPlanes.php?proceso=eliminarPlan&id=".$row["id"]."' class='icon'>
-                                <i class='fa-solid fa-trash btn__delete'></i>
-                            </a>
+                            <span class='icon bottom-edit' id='" .$row["id"]. "'>
+                            <i class='fa-solid fa-pen-to-square btn__edit'></i>
+                            </span>
+
+                            <span class='icon bottom-delete' id='" .$row["id"]. "'>
+                            <i class='fa-solid fa-trash btn__delete'></i>
+                            </span>
                         </div>
                     </td>
                 </tr>";
@@ -70,8 +71,31 @@
         $id = $_GET["id"];
         $datos = $formPlanes->eliminarFila($id);
 
-        if($datos == "1"){
-            header("Location: ../vistas/planes.php");
+        if($datos == '1'){
+            echo "1";
+        }else{
+            echo "0";
         }
+
+        unset($datos);
     }
+
+    if($_GET["proceso"] == "editarPlan"){
+
+        $id = $_GET["id"];
+        $nombre = $_GET["nombre"];
+        $costo = $_GET["costo"];
+        $duracion = $_GET["duracion"];
+        $descripcion = $_GET["descripcion"];
+        
+        $datos = $formPlanes ->editarPlan($id,$nombre,$costo,$duracion,$descripcion);
+        
+        if($datos == '1'){
+            echo "1";
+        }else{
+            echo "0";
+        }
+
+        unset($datos);
+        unset($id);
 ?>
