44470baea1120d3a5b504b7ff8b8253f5627919e
Bleyomm <bleyomm@gmail.com>
2022-10-23 18:17:53 -0500

    cambio de conexion mysql

--- config/global.php
+++ config/global.php
@@ -1,15 +1,15 @@
 <?php 
 //IP DEL SERVIDOR DE BASE DE DATOS
-define("DB_HOST", "sql10.freesqldatabase.com");
+define("DB_HOST", "mysql.proyecto-gym.com");
 
 //NOMBRE DE LA BASE DE DATOS
-define("DB_NAME", "sql10526981");
+define("DB_NAME", "info_powergym");
 
 //NOMBRE DEL USUARIO DE LA BASE DE DATOS
-define("DB_USERNAME", "sql10526981");
+define("DB_USERNAME", "bleyomm");
 
 //CLAVE DEL USUARIO DE LA BASE DE DATOS
-define("DB_PASSWORD", "1JNVx1GT7s");
+define("DB_PASSWORD", "ellobo031902");
 
 //CODIFICACION DE LOS CARACTERES
 define("DB_ENCODE", "utf8");
