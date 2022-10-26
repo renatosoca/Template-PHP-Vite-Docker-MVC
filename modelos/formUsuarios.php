d75b04db43685bbed8857fb8e090e52a0bb1aa18
Bleyomm <bleyomm@gmail.com>
2022-10-25 22:04:00 -0500

    cambio usuarios sql

--- modelos/formUsuarios.php
+++ modelos/formUsuarios.php
@@ -17,9 +17,10 @@
         }
 
         public function traerDatosTabla(){
-            $sql = "SELECT u.id, u.dni, u.nombre, u.email, u.celular, u.direccion, p.nombre as NombrePlan, r.nombre as NombreRutina from info_usuarios u 
-            INNER JOIN info_planes p ON u.id_plan = p.id
-            INNER JOIN info_rutinas r ON u.id_rutina = r.id";
+            $sql = "SELECT u.id, u.dni, u.nombre, u.email, u.celular, u.direccion, p.nombre as NombrePlan, r.nombre as NombreRutina
+            from info_usuarios u 
+            LEFT JOIN info_planes p ON u.id_plan = p.id
+            LEFT JOIN info_rutinas r ON u.id_rutina = r.id";
 
             return traerDatosTabla($sql);
         }
